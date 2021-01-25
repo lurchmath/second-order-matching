e99a0783de808aa83325d7a43a9c2300243d9155
Nathan Carter <ncarter@bentley.edu>
2021-01-25 15:36:12 -0500

    Fix bug: Couldn't import from npm

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
-export * from './src/matching.js';
+"use strict"
+
+import {
+    setAPI, isExpressionFunction, makeExpressionFunction,
+    isExpressionFunctionApplication, makeExpressionFunctionApplication,
+    canApplyExpressionFunctionApplication, getVariablesIn, occursFree, isFree,
+    applyExpressionFunctionApplication, getNewVariableRelativeTo,
+    replaceWithoutCapture, alphaConvert, alphaEquivalent, betaReduce,
+    makeConstantExpression, makeProjectionExpression, makeImitationExpression,
+    CASES, Constraint, ConstraintList, MatchingChallenge
+} from './src/matching-without-om.js';
+
+import { API } from './src/openmath-api.js';
+setAPI( API );
+
+export {
+    API, setAPI, isExpressionFunction, makeExpressionFunction,
+    isExpressionFunctionApplication, makeExpressionFunctionApplication,
+    canApplyExpressionFunctionApplication, getVariablesIn, occursFree, isFree,
+    applyExpressionFunctionApplication, getNewVariableRelativeTo,
+    replaceWithoutCapture, alphaConvert, alphaEquivalent, betaReduce,
+    makeConstantExpression, makeProjectionExpression, makeImitationExpression,
+    CASES, Constraint, ConstraintList, MatchingChallenge
+};
